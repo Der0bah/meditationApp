21a97cd70a633ee3b80be38118ac338cbfbe34d8
Der0bah <derobah@me.com>
2025-08-31 10:29:17 -0300

    Add files via upload

---
+++ theme/colors.ts
@@ -0,0 +1,16 @@
+
+export const theme = {
+  bg: "#ffffff",
+  card: "#ffffff",
+  popover: "#ffffff",
+  text: "#020817",
+  mutetext: "#6b7280",
+  primary: "#352461",
+  onPrimary: "#ffffff",
+  secondary: "#f3f1f9",
+  onSecondary: "#352461",
+  border: "#e5e7eb",
+  ring: "#3b82f6",
+  accent: "#ff7e58",
+  dangerLight: "#FFE4E7",
+};
---
+++ theme/typography.ts
@@ -0,0 +1,7 @@
+
+export const fonts = {
+  regular: "Inter_400Regular",
+  semibold: "Inter_600SemiBold",
+  bold: "Inter_700Bold",
+  extrabold: "Inter_800ExtraBold",
+};
